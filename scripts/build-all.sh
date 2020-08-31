71c1e363ce09bddd60ea917cccaf8e1b7240451b
arrowtype <arrowtypeco@gmail.com>
2020-08-31 15:37:03 -0400

    build all fonts with improved glyf coordinates

---
+++ scripts/build-all.sh
@@ -0,0 +1,20 @@
+# !/bin/bash
+
+# A basic script to build all premade onfigs in the project for release
+
+set -e
+
+version=1.062
+release=ArrowType-RecMonoCode-v$version
+
+rm -rf ./fonts
+
+configs=$(ls ./premade-configs)
+
+for config in $configs; do
+    python scripts/instantiate-code-fonts.py ./premade-configs/$config
+done
+
+cp -r ./fonts ./$release
+
+zip $release.zip -r $release -x .DS_*
